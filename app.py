e9df93b841432f9218f4d23805c6471af7985427
Petr Didur <petr_didur@mail.ru>
2023-01-15 15:12:11 +0300

    feed page problem

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from utils import get_posts_all, get_comments_by_post_id
+from flask import Flask, render_template, request
+from utils import get_posts_all, get_comments_by_post_id, get_post_by_pk, search_for_posts, get_posts_by_user
 PATH_POSTS = 'data/posts.json'
 
 app = Flask(__name__)
@@ -10,10 +10,28 @@ def index():  # put application's code here
     posts = get_posts_all(PATH_POSTS)
     return render_template('index.html', posts=posts)
 
+
 @app.route('/posts/<int:postid>')
 def posts_page(postid):
+    posts = get_posts_all(PATH_POSTS)
     comments = get_comments_by_post_id(postid)
-    return render_template('post.html', comments=comments)
+    post = get_post_by_pk(postid)
+    comment_len = len(comments)
+    return render_template('post.html', comments=comments, post=post, posts=posts, comment_len=comment_len)
+
+@app.route('/search/')
+def search_page():
+    s = request.args.get('s')
+    post_list_by_query = search_for_posts(s)
+    len_post_list = len(post_list_by_query)
+    return render_template('search.html', post_list_by_query=post_list_by_query, len_post_list=len_post_list)
+
+@app.route('/users/<username>')
+def user_feed_page(username):
+    s = request.args.get(username)
+    posts_by_name = get_posts_by_user(s)
+    return render_template('user-feed.html', posts_by_name=posts_by_name, username=username)
+
 
 
 
--- utils.py
+++ utils.py
@@ -4,32 +4,41 @@ import json
 def get_posts_all(path_posts):
         with open(path_posts, 'r', encoding='utf-8') as file:
             data_posts = json.load(file)
-            return data_posts
+        return data_posts
 
-
-
+#test = 'johnny'
 def get_posts_by_user(user_name):
         user_posts = []
         for post in get_posts_all(path_posts='data/posts.json'):
-                if user_name == post['poster_name']:
-                    user_posts.append(post)
-        if not user_posts:
-            return ValueError, user_posts
+            if post['poster_name'].lower() == user_name:
+                user_posts.append(post)
+        return user_posts
+
+#print(get_posts_by_user(test))
+
+
+
 
 def get_comments_by_post_id(post_id):
     user_comments = []
-    for post in get_posts_all(path_posts='data/comments.json'):
-        if post_id == post['id']:
-            user_comments.append(post['comment'])
-    if not user_comments:
-        return ValueError, user_comments
+    for comment in get_posts_all(path_posts='data/comments.json'):
+        if comment['post_id'] == post_id:
+            user_comments.append(comment)
+    if user_comments == []:
+        return ValueError
+    else:
+        return user_comments
+
+
 
 def search_for_posts(query):
     post_list_by_query = []
     for post in get_posts_all(path_posts='data/posts.json'):
-        if query in post['content']:
+        if query.lower() in post['content'].lower():
             post_list_by_query.append(post)
     return post_list_by_query
+
+
 
 def get_post_by_pk(pk):
     for post in get_posts_all(path_posts='data/posts.json'):
